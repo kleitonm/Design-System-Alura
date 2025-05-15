167cee9781c3c69467d47d73041bc1d164bc46ca
Kleiton Mendes <kleiton_mendes@hotmail.com>
2025-05-15 19:29:04 -0300

    Add files via upload
    start project

---
+++ VollmedUI/Example/VollmedUI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VollmedUI
+//
+//  Created by Kleiton on 05/08/2025.
+//  Copyright (c) 2025 Kleiton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
